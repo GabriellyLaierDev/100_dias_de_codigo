5ddf876b2bb1d193fc980351c0e5afdcb86d6a08
GabriellyLaierDev <gabrielly.laierl@gmail.com>
2024-09-06 18:11:34 -0300

    Dia 2

---
+++ Dia_2/dia2.py
@@ -0,0 +1,5 @@
+num_um= float(input("Por favor digite um número: "))
+num_dois = float(input("Por favor digite outro número: "))
+soma = num_um + num_dois
+
+print(f"A soma dos dois números é: " , soma)
