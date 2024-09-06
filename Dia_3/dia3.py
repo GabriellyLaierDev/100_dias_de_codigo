89c32800ae960ad60ebd7e4cde6fc78c5a208b65
GabriellyLaierDev <gabrielly.laierl@gmail.com>
2024-09-06 18:13:10 -0300

    Dia 3

---
+++ Dia_3/dia3.py
@@ -0,0 +1,6 @@
+print("Escolha um número para descobrir se ele é par ou ímpar!")
+num = int(input("Digite o número de sua escolha: "))
+if num % 2 == 0:
+    print("Este número é par")
+else:
+    print("Este número é ímpar")
