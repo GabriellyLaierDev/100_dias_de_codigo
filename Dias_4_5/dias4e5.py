97b790880aa09f5fb9ad649f2c4310245cc7ec78
GabriellyLaierDev <gabrielly.laierl@gmail.com>
2024-09-09 10:34:25 -0300

    Dias 4 e 5

---
+++ Dias_4_5/dias4e5.py
@@ -0,0 +1,38 @@
+def adicao(x,y):
+    return x+y
+def subtracao(x,y):
+    return x-y
+def divisao(x,y):
+    return x/y
+def multiplicacao(x,y):
+    return x*y 
+
+def calculadora():
+    print("Selecione a operação")
+    print("1.Adicao")
+    print("2.Subtracao")
+    print("3.Multiplicacao")
+    print("4.Divisao")
+
+while True:
+    escolha= input("Escolha 1/2/3/4: ")
+    if escolha in ('1', '2' , '3' , '4'):
+        x= int(input("Digite o primeiro número:"))
+        y= int(input("Digite outro número: "))
+        if escolha == '1':
+            print("Resultado: ", adicao(x,y))
+        if escolha == '2':
+            print("Resultado: ", subtracao(x,y))
+        if escolha == '3':
+          print("Resultado: ", multiplicacao(x,y))
+        if escolha == '4':
+            if y != 0:
+                print("Resultado: ", divisao(x,y))
+            else:
+                print("Não é permitida a divisão por zero!")
+
+    continuar = input("Deseja fazer outra operação?(s/n)")
+    if continuar == 'n':
+        print("Calculadora encerrada")
+        break;
+calculadora()
